8f2ce0bf90a428afbb7bc1de5501c5801b902d13
Asyny-big <o737b15@voenmeh.ru>
2026-01-29 19:51:09 +0300

    Update subscriptionBuilder.ts

--- src/modules/subscription/subscriptionBuilder.ts
+++ src/modules/subscription/subscriptionBuilder.ts
@@ -21,7 +21,7 @@ const MOBILE_BYPASS_BLOCK: ReadonlyArray<string> = [
 
 
 
-const PRIMARY_SERVER_DISPLAY_NAME = "🚀 LisVPN — 🇪🇪 Эстония (PRO)";
+const PRIMARY_SERVER_DISPLAY_NAME = "🚀 LisVPN — EE Эстония (PRO)";
 
 export type BuildSubscriptionUser = Readonly<{
   expiresAt?: Date | null;
